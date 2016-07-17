480e4e3290bc7f41ecaa0b9f84e40099cc3e8321
pereb4ik <lehass.2001@gmail.com>
2016-07-17 20:34:26 +0300

    Add files via upload

---
+++ dfs.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class dfs {
+    boolean must = true;
+    int color[];
+    boolean used[];
+    /**
+     * edges - Array list of Array list of edges
+     */
+    void dfs(int v, ArrayList<ArrayList<Integer>> edges) {
+        used[v] = true;
+        for (int i = 0; i < edges.get(v).size(); i++) {
+            if (used[edges.get(v).get(i)] == false) {
+                color[edges.get(v).get(i)] = (color[v]) * (-1);
+                dfs(edges.get(v).get(i), edges);
+            } else {
+                if (color[edges.get(v).get(i)] == color[v]) {
+                    must = false;
+                }
+            }
+        }
+    }
+    /**
+     * dfs с прикрученной раскраской двудолного графа
+     */
+}
