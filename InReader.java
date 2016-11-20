b9d87684afd24ddd567a74b2c8081ac49b7ae95f
Leha <lehass.2001@gmail.com>
2016-11-20 11:47:59 +0300

    Update InReader.java

--- InReader.java
+++ InReader.java
@@ -1,38 +1,31 @@
 /**
  * Created by leha on 26.08.16.
  */
-import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
-
-public class InReader {
+class InReader {
     BufferedReader in;
 
-    InReader(String problemName) throws IOException {
-        String myIn = "input.txt";
-        File tmpIn = new File(myIn);
-        if (!tmpIn.exists()) {
-            if (problemName == null) {
-                in = new BufferedReader(new InputStreamReader(System.in));
-            } else {
-                in = new BufferedReader(
-                        new FileReader(problemName + ".in"));
-            }
-        } else {
-            in = new BufferedReader(new FileReader(myIn));
-        }
+    InReader(String ProblemName) throws IOException {
+        in = new BufferedReader(new FileReader(ProblemName));
+    }
+
+    InReader() throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
     }
 
     StringTokenizer tokenizer = new StringTokenizer("");
 
     void updateTokenizer() throws IOException {
-        String a = "a";
-        while (!tokenizer.hasMoreTokens() && a != null) {
-            a = in.readLine();
+        if (!tokenizer.hasMoreTokens()) {
+            String a = in.readLine();
             if (a != null) {
                 tokenizer = new StringTokenizer(a);
             }
         }
+    }
+
+    boolean hasNext() throws IOException {
+        updateTokenizer();
+        return (tokenizer.hasMoreTokens());
     }
 
     int nextInt() throws IOException {
@@ -40,28 +33,9 @@ public class InReader {
         return Integer.parseInt(tokenizer.nextToken());
     }
 
-    boolean hasNextInt() throws IOException {
-        updateTokenizer();
-        if (tokenizer.hasMoreTokens()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     long nextLong() throws IOException {
         updateTokenizer();
         return Long.parseLong(tokenizer.nextToken());
-    }
-
-    BigInteger nextBigInteger() throws IOException {
-        updateTokenizer();
-        return new BigInteger(tokenizer.nextToken());
-    }
-
-    double nextDouble() throws IOException {
-        updateTokenizer();
-        return Double.parseDouble(tokenizer.nextToken());
     }
 
     String next() throws IOException {
@@ -69,11 +43,8 @@ public class InReader {
         return tokenizer.nextToken();
     }
 
-    String nextLine() throws IOException {
-        return in.readLine();
-    }
-
-    void close() throws IOException {
-        in.close();
+    double nextDouble() throws IOException {
+        updateTokenizer();
+        return Double.parseDouble(tokenizer.nextToken());
     }
 }
