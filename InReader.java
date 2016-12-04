2148fbbc24a1d60ec7054b64b6bd744adc2e6809
Leha <lehass.2001@gmail.com>
2016-12-04 20:43:10 +0300

    Update InReader.java

--- InReader.java
+++ InReader.java
@@ -1,50 +1,22 @@
-/**
- * Created by leha on 26.08.16.
- */
-class InReader {
+class Reader {
     BufferedReader in;
-
-    InReader(String ProblemName) throws IOException {
-        in = new BufferedReader(new FileReader(ProblemName));
-    }
-
-    InReader() throws IOException {
+    Reader() throws IOException{
         in = new BufferedReader(new InputStreamReader(System.in));
     }
-
-    StringTokenizer tokenizer = new StringTokenizer("");
-
-    void updateTokenizer() throws IOException {
-        if (!tokenizer.hasMoreTokens()) {
+    Reader(String name) throws IOException{
+        in = new BufferedReader(new FileReader(name));
+    }
+    StringTokenizer tokin = new StringTokenizer("");
+    void updtokin() throws IOException {
+        if(!tokin.hasMoreTokens()){
             String a = in.readLine();
-            if (a != null) {
-                tokenizer = new StringTokenizer(a);
+            if(a != null){
+                tokin = new StringTokenizer(a);
             }
         }
     }
-
-    boolean hasNext() throws IOException {
-        updateTokenizer();
-        return (tokenizer.hasMoreTokens());
-    }
-
-    int nextInt() throws IOException {
-        updateTokenizer();
-        return Integer.parseInt(tokenizer.nextToken());
-    }
-
-    long nextLong() throws IOException {
-        updateTokenizer();
-        return Long.parseLong(tokenizer.nextToken());
-    }
-
     String next() throws IOException {
-        updateTokenizer();
-        return tokenizer.nextToken();
-    }
-
-    double nextDouble() throws IOException {
-        updateTokenizer();
-        return Double.parseDouble(tokenizer.nextToken());
+        updtokin();
+        return tokin.nextToken();
     }
 }
