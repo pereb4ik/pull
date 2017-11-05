80bfc4510a6b55819947ea7335711292d2e2a1fc
Leha <lehass.2001@gmail.com>
2017-11-05 07:45:13 +0300

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -6,21 +6,16 @@ class Reader {
     Reader(String name) throws IOException{
         in = new BufferedReader(new FileReader(name));
     }
+    
     StringTokenizer tokin = new StringTokenizer("");
-    void updtokin() throws IOException {
-        if(!tokin.hasMoreTokens()){
-            String a = in.readLine();
-            if(a != null){
-                tokin = new StringTokenizer(a);
-            }
-        }
-    }
+    
     String next() throws IOException {
-        updtokin();
+        if(!tokin.hasMoreTokens()){
+            tokin = new StringTokenizer(in.readLine());
+        }
         return tokin.nextToken();
     }
     int nextInt() throws IOException {
-        updtokin();
-        return Integer.parseInt(tokin.nextToken());
+        return Integer.parseInt(next());
     }
 }
